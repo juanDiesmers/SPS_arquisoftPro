c7438b543f409dad73e81011556aa4507f3ef194
juandiesmers <juandiegopala_@outlook.es>
2026-05-20 10:47:43 -0500

    correccion del gateway

--- gateway-service/src/main/java/com/sps/gateway/config/RouteConfig.java
+++ gateway-service/src/main/java/com/sps/gateway/config/RouteConfig.java
@@ -2,10 +2,8 @@ package com.sps.gateway.config;
 
 import com.sps.gateway.filters.JwtAuthenticationFilter;
 import com.sps.gateway.filters.RateLimitGatewayFilter;
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,26 +18,30 @@ public class RouteConfig {
 
         return builder.routes()
 
-                .route("auth-service", r -> r.path("/auth/**")
-                        .uri("lb://auth-service"))
+                .route("auth-service", r -> r
+                        .path("/auth/**")
+                        .uri("http://auth-service:8081"))
 
-                .route("catalog-service", r -> r.path("/planes/**")
+                .route("catalog-service", r -> r
+                        .path("/planes/**")
                         .filters(f -> f
                                 .filter(jwtFilter.apply(new JwtAuthenticationFilter.Config()))
                                 .filter(rateLimitFilter.apply(new RateLimitGatewayFilter.Config())))
-                        .uri("lb://catalog-service"))
+                        .uri("http://catalog-service:8082"))
 
-                .route("purchase-service", r -> r.path("/compras/**")
+                .route("purchase-service", r -> r
+                        .path("/compras/**")
                         .filters(f -> f
                                 .filter(jwtFilter.apply(new JwtAuthenticationFilter.Config()))
                                 .filter(rateLimitFilter.apply(new RateLimitGatewayFilter.Config())))
-                        .uri("lb://purchase-service"))
+                        .uri("http://purchase-service:8083"))
 
-                .route("saludpay-service", r -> r.path("/api/**")
+                .route("saludpay-service", r -> r
+                        .path("/api/**")
                         .filters(f -> f
                                 .filter(jwtFilter.apply(new JwtAuthenticationFilter.Config()))
                                 .filter(rateLimitFilter.apply(new RateLimitGatewayFilter.Config())))
-                        .uri("lb://saludpay"))
+                        .uri("http://saludpay:8086"))
 
                 .build();
     }
